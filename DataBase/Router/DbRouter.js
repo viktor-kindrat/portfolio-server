1154e4d059b2ebeccbe6b4d9ec88c66215164106
Viktor Kindrat <84918090+viktor-kindrat@users.noreply.github.com>
2023-05-15 18:52:47 +0300

    Update DbRouter.js

--- DataBase/Router/DbRouter.js
+++ DataBase/Router/DbRouter.js
@@ -3,9 +3,10 @@ const controller = require("../Controller/DbController");
 
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
-
+const cors = require('cors');
 
 const router = new Router();
+router.use(cors())
 
 router.get("/getSkills", controller.getSkills)
 router.get("/getProjects", controller.getProjects)
