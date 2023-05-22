41ea62292bb1ec5a8f665d500a588f56d69bf138
Viktor Kindrat <kindratvictor07@gmail.com>
2023-05-22 22:13:16 +0300

    telegram API connect

--- Controllers/AppController.js
+++ Controllers/AppController.js
@@ -1,5 +1,18 @@
+require("dotenv").config()
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.TELEGRAM_TOKEN;
+
+const bot = new TelegramBot(token, {polling: true});
+
 class Controller {
     getDefault(req, res) {
+        res.json({ status: "ok" }).status(200)
+    }
+    sendMessage(req, res) {
+        let data = req.body;
+        bot.sendMessage(process.env.TELEGRAM_ID, `<b>NEW message</b>\n\n<b>${data.name}</b>\n${data.message}`, {
+            parse_mode: 'HTML'
+        })
         res.json({ status: "ok" }).status(200)
     }
 }
--- Routers/AppRouter.js
+++ Routers/AppRouter.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const Controller = require("../Controllers/AppController")
+const Controller = require("../Controllers/AppController");
+const cors = require("cors")
 
 let Router = express();
+Router.use(cors())
 
 Router.get("/", Controller.getDefault);
+Router.post("/sendMessage", Controller.sendMessage);
 
 module.exports = Router
